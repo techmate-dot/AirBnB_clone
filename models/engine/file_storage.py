ead1505a79c588e97b7f9c51208f48bd50aac2ab
techmate-dot <snwankwo218@gmail.com>
2022-08-06 16:38:04 +0100

    make it pycode compliant

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -24,13 +24,12 @@ class FileStorage:
         Args:
             obj (obj): New object that will be save in __objects
         """
-        FileStorage.__objects[obj["id"]] = obj  # Todo: change the key to "<class name>.id" e.g "Basemode.121212"
+    # Todo: change the key to "<class name>.id" e.g "Basemode.121212"
+        FileStorage.__objects[obj["id"]] = obj
 
     def save(self):
         """ Save the __objects to a file.json
         """
-        
-        
         with open(FileStorage.__file_path, 'w', encoding='UTF-8') as json_file:
             if len(FileStorage.__objects) != 0:
                 json.dump(FileStorage.__objects, json_file, indent=4)
@@ -44,10 +43,3 @@ class FileStorage:
                 FileStorage.__objects = json.load(json_file)
         except FileNotFoundError:
             pass
-
-
-file = FileStorage()
-file.new({"id": "hello,wold"})
-file.new({"id": "code"})
-file.reload()
-print(file.all())
