d20427308e7eda7e9ab71f32859a3c77ea243c95
techmate-dot <snwankwo218@gmail.com>
2022-08-07 17:56:36 +0100

    checking

--- models/base_model.py
+++ models/base_model.py
@@ -31,8 +31,8 @@ class BaseModel:
         """returns a dict representation the class"""
 
         new_dict = {'__class__': 'BaseModel'}
-        self.created_at = self.created_at
-        self.updated_at = self.updated_at
+        self.created_at = str(self.created_at)
+        self.updated_at = str(self.updated_at)
         return {**new_dict, **self.__dict__}
 
     def __str__(self) -> str:
