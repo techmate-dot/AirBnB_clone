f783903125b5fd4700f56be3787df21f4907fedd
techmate-dot <snwankwo218@gmail.com>
2022-08-07 15:53:29 +0100

    udated base model

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,7 @@ class BaseModel:
     def __init__(self, *args, **kwargs) -> None:
         """Assigns all instance attributes
         """
-        DateTime = datetime.datetime.now().isoformat()
+        DateTime = datetime.datetime.now()
         self.created_at = DateTime
         self.id = str(uuid.uuid4())
         self.updated_at = DateTime
@@ -30,10 +30,12 @@ class BaseModel:
 
     def to_dict(self):
         """returns a dict representation the class"""
-        new_dict = {'__class__': 'BaseModel'}
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.updated_at
-        return {**new_dict, **self.__dict__}
+
+        new_dict = self.__dict__
+        new_dict['__class__'] = 'BaseModel'
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
 
     def __str__(self) -> str:
         """Returns a string representation of the class"""
