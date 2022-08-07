dff08b70de2c4ac3abbae8610bd0ca2c21d2ae8e
techmate-dot <snwankwo218@gmail.com>
2022-08-07 11:25:13 +0100

    up

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -11,3 +11,25 @@ class TestBaseModel(unittest.TestCase):
     Args:
         unittest (object): test object
     """
+    def test_created_at(self):
+        """Ensure that the created_at is of type Datetime and
+            is the same as current time"""
+        my_model = BaseModel()
+        self.assertEqual(type(my_model.created_at).__name__, 'datetime')
+
+    def test_updated_at(self):
+        """Ensure that the updated_at is of type Datetime and is
+            not equals created_at
+        """
+        my_model = BaseModel()
+        my_model.save()
+        self.assertEqual(type(my_model.updated_at).__name__, 'datetime')
+        self.assertFalse(my_model.created_at is my_model.updated_at)
+
+    def test_new_instance(self):
+        """Check to see that a new instance has been made
+        """
+        my_model = BaseModel()
+        my_new_model = BaseModel(my_model.to_dict)
+        self.assertFalse(my_model is my_new_model)
+        self.assertFalse(my_model.id is my_new_model.id)
