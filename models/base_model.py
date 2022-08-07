06cf27750ec448626a263f99e94736202265de7e
techmate-dot <snwankwo218@gmail.com>
2022-08-07 19:18:34 +0100

    checking

--- models/base_model.py
+++ models/base_model.py
@@ -29,11 +29,12 @@ class BaseModel:
 
     def to_dict(self):
         """returns a dict representation the class"""
-        new_dict = self.__dict__
-        new_dict['__class__']: 'BaseModel'
-        new_dict['created_at'] = self.created_at.isoformat()
-        new_dict['updated_at'] = self.updated_at.isoformat()
-        return new_dict
+        bm_dict = dict(self.__dict__)
+        bm_dict['__class__'] = type(self).__name__
+        bm_dict['created_at'] = bm_dict['created_at'].isoformat()
+        bm_dict['updated_at'] = bm_dict['updated_at'].isoformat()
+        return bm_dict
+
     def __str__(self) -> str:
         """Returns a string representation of the class"""
 
