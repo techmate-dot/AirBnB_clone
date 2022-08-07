bc8d9461fb24eb894304af7d24cbf31da935cd54
techmate-dot <snwankwo218@gmail.com>
2022-08-07 18:14:31 +0100

    checking

--- models/base_model.py
+++ models/base_model.py
@@ -26,7 +26,7 @@ class BaseModel:
         """updates the public instance attribute
             updated_at with the current datetime
         """
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now().isoformat()
 
     def to_dict(self):
         """returns a dict representation the class"""
