61cf379d2ee08fe826c9b003f3b33187c000c6e7
techmate-dot <snwankwo218@gmail.com>
2022-08-07 16:16:43 +0100

    checking

--- models/base_model.py
+++ models/base_model.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
-"""A class BaseModel that defines all
-    common attributes/methods for other classes
-   """
+"""This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """A template that defines all common
-        attributes/methods for other classes:
-    """
+    """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
-        """Assigns all instance attributes
-        """
-        self.created_at =  datetime.now()
-        self.id = str(uuid.uuid4())
-        self.updated_at = self.created_at
-        if kwargs:
-            kwargs.pop("__class__")
-            for keys in kwargs:
-                self.keys = kwargs[keys]
+        """Instatntiates a new model"""
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
+
+    def __str__(self):
+        """Returns a string representation of the instance"""
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
-        """updates the public instance attribute
-            updated_at with the current datetime
-        """
+        """Updates updated_at with current time when instance is changed"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
@@ -36,7 +38,3 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
-
-    def __str__(self) -> str:
-        """Returns a string representation of the class"""
-        return f"[BaseModel] ({self.id}) {self.__dict__})"
