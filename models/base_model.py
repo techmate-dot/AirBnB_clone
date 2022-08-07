013b50f9952287cb127d7b4522e29b73c9162f6e
techmate-dot <snwankwo218@gmail.com>
2022-08-07 19:11:30 +0100

    checking

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,7 @@ class BaseModel:
     def __init__(self, *args, **kwargs) -> None:
         """Assigns all instance attributes
         """
-        self.created_at = datetime.datetime.now().isoformat()
+        self.created_at = datetime.datetime.now()
         self.id = str(uuid.uuid4())
         self.updated_at = self.created_at
         if kwargs:
@@ -31,7 +31,7 @@ class BaseModel:
         """returns a dict representation the class"""
         new_dict = self.__dict__
         new_dict['__class__']: 'BaseModel'
-        new_dict['created_at'] = self.created_at
+        new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
         return new_dict
     def __str__(self) -> str:
