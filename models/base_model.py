0929c8c1a0a0846bf922f38888712e0fa0304f0f
techmate-dot <snwankwo218@gmail.com>
2022-08-08 14:22:22 +0100

    check

--- models/base_model.py
+++ models/base_model.py
@@ -38,11 +38,26 @@ class BaseModel:
     def to_dict(self):
         """returns a dict representation the class"""
         new_dict = dict(self.__dict__)
-        new_dict['__class__'] = self.__class__.__name__
+        new_dict['__class__'] = 'BaseModel'
         new_dict['created_at'] = self.updated_at.strftime(time_format)
         new_dict['updated_at'] = self.created_at.strftime(time_format)
-        return new_dict
+        yield new_dict
 
     def __str__(self) -> str:
         """Returns a string representation of the class"""
-        return f"[{self.__class__.__name__}]] ({self.id}) {self.__dict__})"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__})"
+
+
+
+
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
