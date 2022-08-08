d6165ea1cb85a6649cf1f73a8a525819f2341991
techmate-dot <snwankwo218@gmail.com>
2022-08-08 14:08:59 +0100

    check

--- models/base_model.py
+++ models/base_model.py
@@ -26,7 +26,7 @@ class BaseModel:
                     setattr(self, keys, kwargs[keys])
         if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now().isoformat()
+            self.created_at = datetime.now()
             self.updated_at = self.created_at
 
     def save(self):
@@ -37,8 +37,8 @@ class BaseModel:
 
     def to_dict(self):
         """returns a dict representation the class"""
-        new_dict = dict(self.__dict__)
-        new_dict['__class__'] = type(self).__name__
+        new_dict = {'__class__': 'base_model'}
+        new_dict.update(dict(self.__dict__))
         new_dict['created_at'] = self.updated_at.strftime(time_format)
         new_dict['updated_at'] = self.created_at.strftime(time_format)
         return new_dict
