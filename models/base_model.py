0fbb442dd28ee422a68b95a676ebfae378b56945
techmate-dot <snwankwo218@gmail.com>
2022-08-08 01:32:02 +0100

    check

--- models/base_model.py
+++ models/base_model.py
@@ -24,7 +24,6 @@ class BaseModel:
                     setattr(self, keys, datetime.strptime(values, time_format))
                 else:
                     setattr(self, keys, kwargs[keys])
-            print(kwargs)
         else:
             self.created_at = datetime.now()
             self.updated_at = self.created_at
@@ -46,19 +45,3 @@ class BaseModel:
     def __str__(self) -> str:
         """Returns a string representation of the class"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__})"
-
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-print(my_model.id)
-print(my_model)
-print(type(my_model.created_at))
-print("--")
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
-
-print("--")
-my_new_model = BaseModel(**my_model_json)
