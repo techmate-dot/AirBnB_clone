c6004dcdbdaa802930d96488b499467aab8b60cc
techmate-dot <snwankwo218@gmail.com>
2022-08-08 14:23:47 +0100

    check

--- models/base_model.py
+++ models/base_model.py
@@ -46,18 +46,3 @@ class BaseModel:
     def __str__(self) -> str:
         """Returns a string representation of the class"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__})"
-
-
-
-
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
